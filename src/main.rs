9d5564e92aad9404f84cb42517a554fcc2cb988c
Brooklyn Zelenka <be.zelenka@gmail.com>
2018-11-27 09:30:08 -0800

    Match all nested paths

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+use std::path::PathBuf;
+use rocket::response::Redirect;
+
 #[macro_use]
 extern crate rocket;
 
-use rocket::response::Redirect;
-
-#[get("/<path>")]
-fn path(path: String) -> Redirect {
-    Redirect::to(format!("https://ethereum-magicians.org/{}", path))
+#[get("/<path..>")]
+fn path(path: PathBuf) -> Redirect {
+    let path_str = path.into_os_string().into_string().unwrap();
+    Redirect::to(format!("https://ethereum-magicians.org/{}", path_str))
 }
 
 #[get("/")]
